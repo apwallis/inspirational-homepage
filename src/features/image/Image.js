bbf3e36cb36bd7eaff365fc2468ad643d540ad69
apwallis <apwallis@googlemail.com>
2024-06-18 11:45:58 +0100

    Created and Implemented Image

---
+++ src/features/image/Image.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { selectImage } from "./imageSlice";
+
+
+export default function Image() {
+    const imageUrl = useSelector(selectImage);
+  
+    return (
+        <div style={{ 
+            backgroundImage: `url("${imageUrl}")` 
+          }}>
+            
+        </div>
+    );
+  }
