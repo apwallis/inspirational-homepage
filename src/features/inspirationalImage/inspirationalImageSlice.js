0455787d538557af36f64f8ddf199200aa25daab
apwallis <apwallis@googlemail.com>
2024-06-18 11:28:21 +0100

    imageSlice Complete

---
+++ src/features/inspirationalImage/inspirationalImageSlice.js
@@ -0,0 +1,37 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const inspirationalImageSlice = createSlice({
+  name: "inspirationalImage",
+  initialState: {
+    imageUrls: [
+        'https://images.unsplash.com/photo-1433086966358-54859d0ed716?crop=entropy&cs=srgb&fm=jpg&ixid=M3wxMDI0NTh8MHwxfHNlYXJjaHwxfHxuYXR1cmV8ZW58MHx8fHwxNzE4NzAxNjUyfDA&ixlib=rb-4.0.3&q=85',
+        'https://images.unsplash.com/photo-1469474968028-56623f02e42e?crop=entropy&cs=srgb&fm=jpg&ixid=M3wxMDI0NTh8MHwxfHNlYXJjaHwyfHxuYXR1cmV8ZW58MHx8fHwxNzE4NzAxNjUyfDA&ixlib=rb-4.0.3&q=85',
+        'https://images.unsplash.com/photo-1470071459604-3b5ec3a7fe05?crop=entropy&cs=srgb&fm=jpg&ixid=M3wxMDI0NTh8MHwxfHNlYXJjaHwzfHxuYXR1cmV8ZW58MHx8fHwxNzE4NzAxNjUyfDA&ixlib=rb-4.0.3&q=85',
+        'https://images.unsplash.com/photo-1426604966848-d7adac402bff?crop=entropy&cs=srgb&fm=jpg&ixid=M3wxMDI0NTh8MHwxfHNlYXJjaHw0fHxuYXR1cmV8ZW58MHx8fHwxNzE4NzAxNjUyfDA&ixlib=rb-4.0.3&q=85',
+        'https://images.unsplash.com/photo-1501854140801-50d01698950b?crop=entropy&cs=srgb&fm=jpg&ixid=M3wxMDI0NTh8MHwxfHNlYXJjaHw1fHxuYXR1cmV8ZW58MHx8fHwxNzE4NzAxNjUyfDA&ixlib=rb-4.0.3&q=85'
+      ],
+    currentImageIndex: 0,
+  },
+  reducers: {
+    incrementImageIndex: (state, action) => {
+        const numberOfImages = state.imageUrls.length - 1; 
+        if (state.currentImageIndex === numberOfImages) {
+            state.currentImageIndex = 0;
+        } else {
+            state.currentImageIndex = state.currentImageIndex + 1;
+        }
+    },
+    decrementImageIndex: (state, action) => {
+        const numberOfImages = state.imageUrls.length - 1;
+        if (state.currentImageIndex === 0) {
+            state.currentImageIndex = numberOfImages;
+        } else {
+            state.currentImageIndex = state.currentImageIndex - 1;
+        }
+    },
+  },
+});
+
+export const { incrementImageIndex, decrementImageIndex } = inspirationalImageSlice.actions;
+export const selectImage = (state) => state.inspirationalImage.imageUrls[state.inspirationalImage.currentImageIndex];
+export default topicsSlice.reducer;
