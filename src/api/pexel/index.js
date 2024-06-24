ffb996742d13eebe232b9b99f48ea028c56a5829
apwallis <apwallis@googlemail.com>
2024-06-24 11:58:19 +0100

    Api access bugfix 1

--- src/api/pexel/index.js
+++ src/api/pexel/index.js
@@ -1,5 +1,6 @@
 
 const API_URL = `https://api.pexels.com/v1/search?query=nature`;
+const API_KEY = 'rQep8aHmyWrErlBrDkFUjw8CkRfgbAJJx3nohydtgQEpdvMEfY2GFiTD';
 
 export const getImages = async () => {
     const response = await fetch(API_URL,
@@ -7,7 +8,7 @@ export const getImages = async () => {
             method: "GET",
             headers: {
                 Accept: "application/json",
-                Authorization: process.env.PEXEL_IMAGE_API_KEY,     //use the apikey you have generated
+                Authorization: API_KEY,     //use the apikey you have generated
             },
         }
     );
